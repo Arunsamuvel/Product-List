ebd26d6b2ac26e99f8832f79aa6f787c31b1d13b
Arunsamuvel <arun15ee03@gmail.com>
2024-08-16 23:08:59 +0530

    Media-Query added to the first component

--- vite-project/src/Component/Container/PriceCart.jsx
+++ vite-project/src/Component/Container/PriceCart.jsx
@@ -2,14 +2,16 @@ import "./pricecart.css";
 import image1 from "../assets/image-baklava-desktop.jpg";
 import image2 from "../assets/image-baklava-mobile.jpg";
 import image3 from "../assets/image-baklava-tablet.jpg";
+import image4 from "../assets/image-baklava-thumbnail.jpg";
 
 const PriceCart = () => {
   return (
     <div className="box">
-      <h1>hello world!</h1>
-      <img src={image1} alt="" />
-      <img src={image2} alt="" />
-      <img src={image3} alt="" />
+      {/* <h1 className="h1">hello world!</h1> */}
+      <img src={image1} alt="" className="desktop" />
+      <img src={image2} alt="" className="tablet" />
+      <img src={image3} alt="" className="mobile" />
+      <img src={image4} className="preview" />
     </div>
   );
 };
